189ea3f07673d57088d58f281189d868511278f6
Mestre Verde <6carlosgomes4@gmail.com>
2026-06-05 15:15:24 +0100

    Changes to be committed: 	modified:   src/main.c 	modified:   utils/aux_func.c         função para mostrar os caracteres agora veio para aqui.

--- src/main.c
+++ src/main.c
@@ -3,52 +3,8 @@
 #include "main_menu.h"
 #include "aux_func.h"
 
-void printCharTable(void)
-{
-    printf("Char   | Dec | Hex\n");
-    printf("-------------------\n");
-
-    for (int i = 0; i < 128; i++)
-    {
-        switch (i)
-        {
-        case '\0':
-            printf("\\0     | %3d | %02X\n", i, i);
-            break;
-        case '\a':
-            printf("\\a     | %3d | %02X\n", i, i);
-            break;
-        case '\b':
-            printf("\\b     | %3d | %02X\n", i, i);
-            break;
-        case '\t':
-            printf("\\t     | %3d | %02X\n", i, i);
-            break;
-        case '\n':
-            printf("\\n     | %3d | %02X\n", i, i);
-            break;
-        case '\v':
-            printf("\\v     | %3d | %02X\n", i, i);
-            break;
-        case '\f':
-            printf("\\f     | %3d | %02X\n", i, i);
-            break;
-        case '\r':
-            printf("\\r     | %3d | %02X\n", i, i);
-            break;
-
-        default:
-            if (i >= 32 && i <= 126)
-                printf("'%c'    | %3d | %02X\n", i, i, i);
-            else
-                printf("CTRL   | %3d | %02X\n", i, i);
-        }
-    }
-}
-
 int main(void)
 {
-
     if (setup())
     {
         LOG_ERROR("Houve um problema com a função \"setup()\"");
--- utils/aux_func.c
+++ utils/aux_func.c
@@ -58,6 +58,49 @@ void clearStdinTrash(void)
     // putchar('\n');
 }
 
+void printCharTable(void)
+{
+    printf("Char   | Dec | Hex\n");
+    printf("-------------------\n");
+
+    for (int i = 0; i < 128; i++)
+    {
+        switch (i)
+        {
+        case '\0':
+            printf("\\0     | %3d | %02X\n", i, i);
+            break;
+        case '\a':
+            printf("\\a     | %3d | %02X\n", i, i);
+            break;
+        case '\b':
+            printf("\\b     | %3d | %02X\n", i, i);
+            break;
+        case '\t':
+            printf("\\t     | %3d | %02X\n", i, i);
+            break;
+        case '\n':
+            printf("\\n     | %3d | %02X\n", i, i);
+            break;
+        case '\v':
+            printf("\\v     | %3d | %02X\n", i, i);
+            break;
+        case '\f':
+            printf("\\f     | %3d | %02X\n", i, i);
+            break;
+        case '\r':
+            printf("\\r     | %3d | %02X\n", i, i);
+            break;
+
+        default:
+            if (i >= 32 && i <= 126)
+                printf("'%c'    | %3d | %02X\n", i, i, i);
+            else
+                printf("CTRL   | %3d | %02X\n", i, i);
+        }
+    }
+}
+
 int readDigitUserInput(const char prompt[], int *var)
 {
 #define BUFFER_LEN (10 + 1)
